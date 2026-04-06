8ffa99c73847fb4a447a498e0554d6b03c4c5f53
joe154 <ojukwuebuka676@gmail.com>
2026-04-06 21:03:18 +0100

    oj

--- components/Layout/MainLayout.tsx
+++ components/Layout/MainLayout.tsx
@@ -7,24 +7,21 @@ import MotionWrapper from '../MotionWrapper';
 import { AuthProvider } from '../AuthProvider';
 import { ConfirmProvider } from '../ConfirmProvider';
 import { ToastProvider } from '../ToastProvider';
-import { ErrorBoundary } from '../ErrorBoundary';
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
+
   return (
-    <ErrorBoundary>
-      <AuthProvider>
-        <ConfirmProvider>
-          <ToastProvider>
-            <div className="min-h-screen flex flex-col">
-              <Header />
-              <MotionWrapper>
-                <main className="flex-1 container-max px-4 sm:px-6 lg:px-8 py-8 sm:py-12">{children}</main>
-              </MotionWrapper>
-              <Footer />
-            </div>
-          </ToastProvider>
-        </ConfirmProvider>
-      </AuthProvider>
-    </ErrorBoundary>
+    <AuthProvider>
+      <ConfirmProvider>
+        <ToastProvider>
+          <div className="min-h-screen flex flex-col">
+            <Header />
+            <MotionWrapper>
+              <main className="flex-1 container-max px-4 sm:px-6 lg:px-8 py-8 sm:py-12">{children}</main>
+            </MotionWrapper>
+            <Footer />
+          </div>
+        </ToastProvider>
+      </ConfirmProvider>
+    </AuthProvider>
   );
 }
